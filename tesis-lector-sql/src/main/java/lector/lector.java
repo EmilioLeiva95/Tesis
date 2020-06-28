11e387e9c96f160931bcf90be9fb2be9bed43054
eleiva <eleiva@ofbox.com>
2020-06-27 22:29:43 -0600

    Cambio en validacion de erores

--- tesis-lector-sql/src/main/java/lector/lector.java
+++ tesis-lector-sql/src/main/java/lector/lector.java
@@ -1,7 +1,6 @@
 package lector;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
